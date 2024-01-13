0859649c713f25c403bdf22174029ab103ce0f0b
Tatsufumi Haneda <haneda.tatsufumi@isid.co.jp>
2024-01-13 22:18:00 +0900

    modified:   index.js 	modified:   package.json

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.get('/api/v1/users', function (req, res) {
   })
 });
 
-app.listen(3000, function () {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
   console.log('Example app listening on port 3000!')
 });
